7f0e2f9f402bffb696f3b1b09e84d27a7a4ad340
bingozb <454113692@qq.com>
2017-04-12 17:39:00 +0800

    日志优化

--- src/main/java/cn/dankal/tools/api/APILogger.java
+++ src/main/java/cn/dankal/tools/api/APILogger.java
@@ -7,6 +7,10 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * API日志管理器
  */
@@ -16,16 +20,25 @@ public class APILogger {
     private Logger logger = Logger.getLogger(this.getClass());
 
     @Pointcut("execution(* cn.dankal.web.service..*(..))")
-    public void apiPointcut() {}
+    public void apiPointcut() {
+    }
 
     @Around("apiPointcut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         Object result = null;
         try {
-            long start = System.currentTimeMillis();
             result = joinPoint.proceed();
-            long takeTime = System.currentTimeMillis() - start;
-            logger.info(joinPoint + " Use time: " + takeTime + "ms");
+            Object obj[] = joinPoint.getArgs();
+            if (obj.length > 0) {
+                APIRequest request = (APIRequest) obj[0];
+                Set set = request.getParams().entrySet();
+                Map.Entry[] entries = (Map.Entry[]) set.toArray(new Map.Entry[set.size()]);
+                for (Map.Entry entry : entries) {
+                    logger.info("[Params] " + entry.getKey() + ":" + entry.getValue());
+                }
+            } else {
+                logger.info("[Params] null");
+            }
         } catch (Throwable e) {
             logger.info(joinPoint + " Exception: " + e.getMessage());
         }
@@ -35,12 +48,12 @@ public class APILogger {
     //后置返回通知
     @AfterReturning(pointcut = "apiPointcut()", argNames = "joinPoint, response", returning = "response")
     public void afterReturn(JoinPoint joinPoint, APIResponse response) {
-        logger.info(joinPoint + " Response: " + new Gson().toJson(response));
+        logger.info(joinPoint + " Response: " + new Gson().toJson(response) + "\n");
     }
 
     //抛出异常后通知
     @AfterThrowing(pointcut = "apiPointcut()", throwing = "ex")
     public void afterThrow(JoinPoint joinPoint, Exception ex) {
-        logger.info(joinPoint + " Exception: " + ex.getMessage());
+        logger.error(joinPoint + " Exception: " + ex.getMessage());
     }
 }
--- src/main/test/cn/dankal/web/service/UserServiceTest.java
+++ src/main/test/cn/dankal/web/service/UserServiceTest.java
@@ -2,8 +2,6 @@ package cn.dankal.web.service;
 
 import cn.dankal.tools.api.APIRequest;
 import cn.dankal.tools.api.APIResponse;
-import com.google.gson.Gson;
-import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,30 +14,34 @@ import javax.annotation.Resource;
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
 public class UserServiceTest {
 
-    private Logger logger = Logger.getLogger(this.getClass());
-
     @Resource
     private UserService userService;
 
     @Test
     public void login() throws Exception {
+        // 正常登录
         APIRequest request = new APIRequest();
         request.setAttribute("username", "bingo");
         request.setAttribute("password", "123456");
         APIResponse response = userService.login(request);
-        assertTrue(response);
+        Assert.assertTrue(response.getMessage(), response.getState().equals("00001"));
+
+        // 密码错误
+        request.setAttribute("username", "bingo");
+        request.setAttribute("password", "1234567");
+        response = userService.login(request);
+        Assert.assertTrue(response.getMessage(), response.getState().equals("00002"));
+
+        // 用户名不存在
+        request.setAttribute("username", "bingo1");
+        request.setAttribute("password", "123456");
+        response = userService.login(request);
+        Assert.assertTrue(response.getMessage(), response.getState().equals("00003"));
     }
 
     @Test
     public void allUsers() throws Exception {
         APIResponse response = userService.allUsers();
-        assertTrue(response);
-    }
-
-    private void assertTrue(APIResponse response) {
-        Gson gson = new Gson();
-        logger.info(gson.toJson(response));
-
         Assert.assertTrue(response.getMessage(), response.getState().equals("00001"));
     }
 }
